9f56f39f0cdf6c7039da438c42e05eb1d854cd60
james linden <j.linden@ngc.com>
2019-01-02 08:24:57 -0500

    initialize empty project

---
+++ README.md
@@ -0,0 +1,10 @@
+# aoc2018
+
+My solutions to Advent of Code 2018
+
+## License
+
+Copyright © 2018 James Linden
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject aoc2018 "0.1.0-SNAPSHOT"
+  :description "Advent of Code 2018 Solutions"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  ;:target-path "target/%s"
+  ;:profiles {:uberjar {:aot :all}}
+  )
